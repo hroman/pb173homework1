2ce72ee2753074f97c567fe7677950a265306cfa
Roman Marecek <marecek.roman@seznam.cz>
2011-10-03 17:35:31 +0200

    Pb173homework1 commit: hello world

---
+++ 01/.tmp_versions/pb173.mod
@@ -0,0 +1,2 @@
+/home/hrom/Dokumenty/skola/pb173linux/pb173/01/pb173.ko
+/home/hrom/Dokumenty/skola/pb173linux/pb173/01/pb173.o
---
+++ 01/pb173.c
@@ -0,0 +1,21 @@
+#include <linux/module.h>
+#include <linux/slab.h>
+
+static int my_init(void)
+{
+	printk(KERN_INFO "Hello world\n");
+	return 0;
+}
+
+static void my_exit(void)
+{
+	char* p = (kmalloc(1000, GFP_KERNEL));
+	strcpy(p, "Bye R");
+	printk(KERN_INFO "%s\n%h\n", p, p);
+	kfree(p);
+}
+
+module_init(my_init);
+module_exit(my_exit);
+
+MODULE_LICENSE("GPL");
